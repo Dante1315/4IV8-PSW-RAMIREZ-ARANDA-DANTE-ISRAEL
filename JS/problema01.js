a3cc854bcc2af53a3f5c85f80b7e6aea7e895595
Dante1315 <ramirez.aranda.dante.israel@gmail.com>
2021-03-27 17:21:06 -0600

    1.1

---
+++ JS/problema01.js
@@ -0,0 +1,19 @@
+function validarn(e){
+    var teclado=(document.all)?e.keyCode:e.which
+    if(teclado==8)return true;
+
+    var patron= /[0-9\d .]/
+
+    var prueba= String.fromCharCode(teclado);
+    return patron.test(prueba)
+}
+
+function interes(){
+    var valor=document.formulario.cantidad.value;
+    var result=parseInt(valor);
+    var interes=result*.02;
+    var total=result+interes;
+
+    document.formulario.sueldoti.value="$"+total;
+
+}
